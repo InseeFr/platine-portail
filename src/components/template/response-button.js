10c6e1ea4c620d4ed016b3a7779c4041cd27605e
y72wvh <betty.becuwe@insee.fr>
2024-06-05 11:11:30 +0200

    Revert "Merge pull request #55 from InseeFr/fix/bouton_open_close"
    This reverts commit 18911f3f329974605d2e813511890b625344cb91, reversing
    changes made to af167f3f24559d8fab5e82a6b58980e997f7d6ae.

--- src/components/template/response-button.js
+++ src/components/template/response-button.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import ReactLoading from 'react-loading';
+import { isSurveyOnLine } from 'utils/api';
 
-const ResponseButton = ({ id, isSurveyOnLine, messageSurveyOffline,messageInfoSurveyOffline}) => {
+const ResponseButton = ({ id,  urlBackEnd}) => {
+  const [surveyOnLine, setSurveyOnLine] = useState(undefined);
+  const [messageSurveyOffline, setMessageSurveyOffline] = useState("");
+  const [messageInfoSurveyOffline, setMessageInfoSurveyOffline] = useState("");
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await isSurveyOnLine(urlBackEnd)(id);
+          if (response.data && response.data.opened === false) {
+            setSurveyOnLine(false);
+            setMessageSurveyOffline(response.data.messageSurveyOffline);
+            setMessageInfoSurveyOffline(response.data.messageInfoSurveyOffline);
+          } else {
+            setSurveyOnLine(true);
+          }
+        } catch (error) {
+          console.error('Error checking survey online:', error);
+          setSurveyOnLine(false);
+        }
+      };
   
-  if(isSurveyOnLine === undefined) {
+      fetchData();
+    }, [id, urlBackEnd]);
+  
+  if(surveyOnLine === undefined) {
     return  (
       <div className="center-block text-center">
         <section style={{display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -20,12 +44,12 @@ const ResponseButton = ({ id, isSurveyOnLine, messageSurveyOffline,messageInfoSu
       <section>
         <h2>{`Répondre à l'enquête`}</h2>
         <Link to={`/${id}/login`} tabIndex="-1">
-          <button type="button" className="btn btn-lg" id="accessButton" disabled={!isSurveyOnLine}>
+          <button type="button" className="btn btn-lg" id="accessButton" disabled={!surveyOnLine}>
             {'Accéder au questionnaire'}
           </button>
         </Link>
         <p />
-        {!isSurveyOnLine && (
+        {!surveyOnLine && (
           <>
             {messageSurveyOffline && <div className="surveyOver">{messageSurveyOffline}</div>}
             {messageInfoSurveyOffline && <p>{messageInfoSurveyOffline}</p>}
